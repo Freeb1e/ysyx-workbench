21011ebf94812ac0434e3036f43511435e179314
Freeb1e <gaoxiao43@163.com>
2026-01-02 20:50:48 +0800

    添加了ref.c的内容

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,15 +19,35 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF) {
+    // NPC -> NEMU: 把 buf 里的数据写入 NEMU 的物理内存 addr 处
+    // 这是一个逐字节拷贝的例子，也可以用 paddr_write 循环
+    uint8_t *byte_buf = (uint8_t *)buf;
+    for (size_t i = 0; i < n; i++) {
+        paddr_write(addr + i, 1, byte_buf[i]); 
+    }
+  } else {
+    // NEMU -> NPC: 把 NEMU 物理内存 addr 处的数据读到 buf 里
+    uint8_t *byte_buf = (uint8_t *)buf;
+    for (size_t i = 0; i < n; i++) {
+        byte_buf[i] = paddr_read(addr + i, 1);
+    }
+  }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+
+  if (direction == DIFFTEST_TO_REF) {
+    // NPC -> NEMU: 强制让 NEMU 的寄存器等于传入的值
+    cpu = *(CPU_state *)dut; 
+  } else {
+    // NEMU -> NPC: 把 NEMU 当前的寄存器状态拷给外部
+    *(CPU_state *)dut = cpu;
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
